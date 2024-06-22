988ee4ea38a758d555f0b953fe77ca95d2836186
Silvio Tisato <4465268+stisa@users.noreply.github.com>
2024-06-22 23:24:40 +0200

    progress, add coreyz

--- klippy/chelper/__init__.py
+++ klippy/chelper/__init__.py
@@ -19,7 +19,7 @@ SSE_FLAGS = "-mfpmath=sse -msse2"
 SOURCE_FILES = [
     'pyhelper.c', 'serialqueue.c', 'stepcompress.c', 'itersolve.c', 'trapq.c',
     'pollreactor.c', 'msgblock.c', 'trdispatch.c',
-    'kin_cartesian.c', 'kin_corexy.c', 'kin_corexz.c', 'kin_delta.c',
+    'kin_cartesian.c', 'kin_corexy.c', 'kin_corexz.c', 'kin_coreyz.c', 'kin_delta.c',
     'kin_deltesian.c', 'kin_polar.c', 'kin_rotary_delta.c', 'kin_winch.c',
     'kin_extruder.c', 'kin_shaper.c', 'kin_idex.c',
 ]
@@ -113,6 +113,10 @@ defs_kin_corexy = """
 
 defs_kin_corexz = """
     struct stepper_kinematics *corexz_stepper_alloc(char type);
+"""
+
+defs_kin_coreyz = """
+    struct stepper_kinematics *coreyz_stepper_alloc(char type);
 """
 
 defs_kin_delta = """
@@ -222,7 +226,7 @@ defs_all = [
     defs_itersolve, defs_trapq, defs_trdispatch,
     defs_kin_cartesian, defs_kin_corexy, defs_kin_corexz, defs_kin_delta,
     defs_kin_deltesian, defs_kin_polar, defs_kin_rotary_delta, defs_kin_winch,
-    defs_kin_extruder, defs_kin_shaper, defs_kin_idex,
+    defs_kin_extruder, defs_kin_shaper, defs_kin_idex, defs_kin_coreyz
 ]
 
 # Update filenames to an absolute path
--- klippy/kinematics/mirlike.py
+++ klippy/kinematics/mirlike.py
@@ -1,6 +1,6 @@
 # Code for handling the kinematics of a mir-like robot, where 4 motors
 # in a hybrid corexy setup control xyz. See also https://github.com/Apsu/Mir
-# 
+#
 #
 # Copyright (C) 2024  Silvio Tisato <silvio@tisato.me>
 #
@@ -8,11 +8,13 @@
 import logging, math
 import stepper
 
+
 class MIRlikeKinematics:
     def __init__(self, toolhead, config):
         # Setup axis rails
-        self.rails = [stepper.LookupMultiRail(config.getsection('stepper_' + n))
-                      for n in 'abcd']
+        self.rails = [
+            stepper.PrinterRail(config.getsection("stepper_" + n)) for n in "abcd"
+        ]
         # CHECKME: endstops are one per motor, but not shared
         # for s in self.rails[1].get_steppers():
         #     self.rails[0].get_endstops()[0][0].add_stepper(s)
@@ -22,45 +24,56 @@ class MIRlikeKinematics:
         #     self.rails[3].get_endstops()[0][0].add_stepper(s)
         # for s in self.rails[3].get_steppers():
         #     self.rails[2].get_endstops()[0][0].add_stepper(s)
-        # hybridcorexy (markforged) has a single one, but hopefully fine with 4?
-        
-        self.rails[0].setup_itersolve('corexz_stepper_alloc', b'-')
-        self.rails[1].setup_itersolve('corexz_stepper_alloc', b'-')
-        self.rails[2].setup_itersolve('corexz_stepper_alloc', b'-')
-        self.rails[3].setup_itersolve('corexz_stepper_alloc', b'-')
-        
+
+        self.rails[0].setup_itersolve("corexz_stepper_alloc", b"-") 
+        self.rails[1].setup_itersolve("corexz_stepper_alloc", b"+")
+        self.rails[2].setup_itersolve("coreyz_stepper_alloc", b"-")
+        self.rails[3].setup_itersolve("coreyz_stepper_alloc", b"+")
+
         for s in self.get_steppers():
             s.set_trapq(toolhead.get_trapq())
             toolhead.register_step_generator(s.generate_steps)
-        config.get_printer().register_event_handler("stepper_enable:motor_off",
-                                                    self._motor_off)
+        config.get_printer().register_event_handler(
+            "stepper_enable:motor_off", self._motor_off
+        )
         # Setup boundary checks
         max_velocity, max_accel = toolhead.get_max_velocity()
         self.max_z_velocity = config.getfloat(
-            'max_z_velocity', max_velocity, above=0., maxval=max_velocity)
+            "max_z_velocity", max_velocity, above=0.0, maxval=max_velocity
+        )
         self.max_z_accel = config.getfloat(
-            'max_z_accel', max_accel, above=0., maxval=max_accel)
+            "max_z_accel", max_accel, above=0.0, maxval=max_accel
+        )
         self.limits = [(1.0, -1.0)] * 3
         ranges = [r.get_range() for r in self.rails]
-        self.axes_min = toolhead.Coord(*[r[0] for r in ranges], e=0.)
-        self.axes_max = toolhead.Coord(*[r[1] for r in ranges], e=0.)
+        self.axes_min = toolhead.Coord(*[r[0] for r in ranges], e=0.0)
+        self.axes_max = toolhead.Coord(*[r[1] for r in ranges], e=0.0)
+
     def get_steppers(self):
         return [s for rail in self.rails for s in rail.get_steppers()]
+
     def calc_position(self, stepper_positions):
         pos = [stepper_positions[rail.get_name()] for rail in self.rails]
         # X = 1/2 (a-b), where a and b are the two opposite motors
         # Y = 1/2 (c-d), where c and d are the two remaining opposite motors
         # Z = 1/4 (a+b+c+d)
-        return [0.5 * (pos[0] - pos[1]), 0.5 * (pos[2] - pos[3]),  0.25 * (pos[1] + pos[2]+pos[2] + pos[3])]
+        return [
+            0.5 * (pos[0] - pos[1]),
+            0.5 * (pos[2] - pos[3]),
+            0.25 * (pos[1] + pos[2] + pos[2] + pos[3]),
+        ]
+
     def set_position(self, newpos, homing_axes):
         for i, rail in enumerate(self.rails):
             rail.set_position(newpos)
             if i in homing_axes:
                 self.limits[i] = rail.get_range()
+
     def note_z_not_homed(self):
         # Helper for Safe Z Home
         # TODO: is this per axis or per stepper?
         self.limits[2] = (1.0, -1.0)
+
     def home(self, homing_state):
         # Each axis is homed independently and in order
         # TODO: maybe home z first?
@@ -78,22 +91,29 @@ class MIRlikeKinematics:
                 forcepos[axis] += 1.5 * (position_max - hi.position_endstop)
             # Perform homing
             homing_state.home_rails([rail], forcepos, homepos)
+
     def _motor_off(self, print_time):
-        self.limits = [(1.0, -1.0)] * 3 # CHECKME: are limits per axis or rail?
+        self.limits = [(1.0, -1.0)] * 3  # CHECKME: are limits per axis or rail?
+
     def _check_endstops(self, move):
         end_pos = move.end_pos
         for i in (0, 1, 2):
-            if (move.axes_d[i]
-                and (end_pos[i] < self.limits[i][0]
-                     or end_pos[i] > self.limits[i][1])):
+            if move.axes_d[i] and (
+                end_pos[i] < self.limits[i][0] or end_pos[i] > self.limits[i][1]
+            ):
                 if self.limits[i][0] > self.limits[i][1]:
                     raise move.move_error("Must home axis first")
                 raise move.move_error()
+
     def check_move(self, move):
         limits = self.limits
         xpos, ypos = move.end_pos[:2]
-        if (xpos < limits[0][0] or xpos > limits[0][1]
-            or ypos < limits[1][0] or ypos > limits[1][1]):
+        if (
+            xpos < limits[0][0]
+            or xpos > limits[0][1]
+            or ypos < limits[1][0]
+            or ypos > limits[1][1]
+        ):
             self._check_endstops(move)
         if not move.axes_d[2]:
             # Normal XY move - use defaults
@@ -101,15 +121,16 @@ class MIRlikeKinematics:
         # Move with Z - update velocity and accel for Z axis
         self._check_endstops(move)
         z_ratio = move.move_d / abs(move.axes_d[2])
-        move.limit_speed(
-            self.max_z_velocity * z_ratio, self.max_z_accel * z_ratio)
+        move.limit_speed(self.max_z_velocity * z_ratio, self.max_z_accel * z_ratio)
+
     def get_status(self, eventtime):
         axes = [a for a, (l, h) in zip("xyz", self.limits) if l <= h]
         return {
-            'homed_axes': "".join(axes),
-            'axis_minimum': self.axes_min,
-            'axis_maximum': self.axes_max,
+            "homed_axes": "".join(axes),
+            "axis_minimum": self.axes_min,
+            "axis_maximum": self.axes_max,
         }
+
 
 def load_kinematics(toolhead, config):
     return MIRlikeKinematics(toolhead, config)
