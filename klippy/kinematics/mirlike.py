3969368b08a035c0561757e80c62e0fb541cc158
Silvio Tisato <4465268+stisa@users.noreply.github.com>
2024-06-23 20:39:59 +0200

    moving in z works

--- klippy/kinematics/mirlike.py
+++ klippy/kinematics/mirlike.py
@@ -5,41 +5,89 @@
 # Copyright (C) 2024  Silvio Tisato <silvio@tisato.me>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
-import logging, math
-import stepper
-
+import logging, math, collections
+import stepper, extras.homing
 
 class MIRlikeKinematics:
     def __init__(self, toolhead, config):
         # Setup axis rails
-        steppers = [config.getsection("stepper_" + n) for n in "abcd"]
-        self.rails = [stepper.PrinterRail(steppers[0]),stepper.PrinterRail(steppers[2]),stepper.PrinterRail(steppers[1])]
-     
+        printer = config.get_printer()
+        ppins = printer.lookup_object('pins')
+            
+        stepper_configs = [config.getsection("stepper_" + n) for n in "abcd"]
+        self.steppers = []
+        self.endstops = []
+        self.homing_infos = []
+        self.ranges = []
+        self.endstop_map = {}
+        
+        for c in stepper_configs:
+            # add stepper
+            self.steppers.append(stepper.PrinterStepper(c))
+            # config endstop
+            endstop_pin = c.get('endstop_pin')
+            pin_params = ppins.parse_pin(endstop_pin, True, True)
+            # Normalize pin name
+            pin_name = "%s:%s" % (pin_params['chip_name'], pin_params['pin'])
+            # register the endstop
+            mcu_endstop = ppins.setup_pin('endstop', endstop_pin)
+            self.endstop_map[pin_name] = {'endstop': mcu_endstop,
+                                        'invert': pin_params['invert'],
+                                        'pullup': pin_params['pullup']}
+            name = self.steppers[-1].get_name(short=True)
+            self.endstops.append((mcu_endstop, name))
+            query_endstops = printer.load_object(c, 'query_endstops')
+            query_endstops.register_endstop(mcu_endstop, name)
+
+            self.endstops[-1][0].add_stepper(self.steppers[-1])
+            pos_endstop = c.getfloat('position_endstop')
+            self.ranges.append((c.getfloat('position_min'),c.getfloat('position_max')))
+            # what a mess... TODO: make my own axis class
+            homing_speed = c.getfloat('homing_speed', 5.0, above=0.)
+            second_homing_speed = c.getfloat(
+                'second_homing_speed', homing_speed/2., above=0.)
+            homing_retract_speed = c.getfloat(
+                'homing_retract_speed', homing_speed, above=0.)
+            homing_retract_dist = c.getfloat(
+                'homing_retract_dist', 5., minval=0.)
+            homing_positive_dir = c.getboolean(
+                'homing_positive_dir', None)
+            if homing_positive_dir is None:
+                axis_len = self.ranges[-1][1] - self.ranges[-1][0]
+                if pos_endstop <= self.ranges[-1][0] + axis_len / 4.:
+                    homing_positive_dir = False
+                elif pos_endstop >= self.ranges[-1][1] - axis_len / 4.:
+                    homing_positive_dir = True
+                else:
+                    raise c.error(
+                        "Unable to infer homing_positive_dir in section '%s'"
+                        % (c.get_name(),))
+                c.getboolean('homing_positive_dir', homing_positive_dir) # TODO: weird?
+            elif ((homing_positive_dir
+                and pos_endstop == self.ranges[-1][1])
+                or (not homing_positive_dir
+                    and pos_endstop == self.ranges[-1][0])):
+                raise c.error(
+                    "Invalid homing_positive_dir / position_endstop in '%s'"
+                    % (c.get_name(),))
+            self.homing_infos.append(collections.namedtuple('homing_info', [
+            'speed', 'position_endstop', 'retract_speed', 'retract_dist',
+            'positive_dir', 'second_homing_speed'])(
+                homing_speed, pos_endstop,
+                homing_retract_speed, homing_retract_dist,
+                homing_positive_dir, second_homing_speed))
         # X uses A and B
-        self.rails[0].get_endstops()[0][0].add_stepper(
-            self.rails[2].get_steppers()[0])
-        #self.rails[0].add_extra_stepper(steppers[1])
-     
         # Y uses C and D
-        self.rails[1].add_extra_stepper(steppers[3])
         # Z uses everything
-        self.rails[2].get_endstops()[0][0].add_stepper(
-            self.rails[0].get_steppers()[0])
-        self.rails[2].get_endstops()[0][0].add_stepper(
-            self.rails[1].get_steppers()[0])
-        self.rails[2].get_endstops()[0][0].add_stepper(
-            self.rails[1].get_steppers()[1])
         
         #self.rails[2].add_extra_stepper(steppers[0]) #a
         #self.rails[2].add_extra_stepper(steppers[2]) #b
         #self.rails[2].add_extra_stepper(steppers[3]) #d
-
-        self.rails[0].setup_itersolve("corexz_stepper_alloc", b"-") 
-        self.rails[1].setup_itersolve("coreyz_stepper_alloc", b"-")
-        self.rails[2].setup_itersolve("cartesian_stepper_alloc", b"-")
-        logging.error("Rails")
-        logging.error(self.rails)
-        logging.error([n.get_name() for n in self.rails[2].get_steppers()])
+        
+        self.steppers[0].setup_itersolve('corexz_stepper_alloc', b'-')
+        self.steppers[1].setup_itersolve('corexz_stepper_alloc', b'+')
+        self.steppers[2].setup_itersolve('coreyz_stepper_alloc', b'-')
+        self.steppers[3].setup_itersolve('coreyz_stepper_alloc', b'+')
         
         for s in self.get_steppers():
             s.set_trapq(toolhead.get_trapq())
@@ -55,10 +103,11 @@ class MIRlikeKinematics:
         self.max_z_accel = config.getfloat(
             "max_z_accel", max_accel, above=0.0, maxval=max_accel
         )
-        self.limits = [(1.0, -1.0)] * 3
-        ranges = [r.get_range() for r in self.rails]
-        self.axes_min = toolhead.Coord(*[r[0] for r in ranges], e=0.0)
-        self.axes_max = toolhead.Coord(*[r[1] for r in ranges], e=0.0)
+        self.limits = [(1.0, -1.0)] * 4
+        # TODO:
+        #ranges = [r.get_range() for r in self.rails]
+        #self.axes_min = toolhead.Coord(*[r[0] for r in ranges], e=0.0)
+        #self.axes_max = toolhead.Coord(*[r[1] for r in ranges], e=0.0)
 
     def get_steppers(self):
         return self.steppers
@@ -77,36 +126,31 @@ class MIRlikeKinematics:
         ]
 
     def set_position(self, newpos, homing_axes):
-        for i, rail in enumerate(self.rails):
-            rail.set_position(newpos)
-            if i in homing_axes:
-                self.limits[i] = rail.get_range()
+        for stepper in self.steppers:
+            stepper.set_position(newpos)
+ 
+        #for i, rail in enumerate(self.rails):
+        #    rail.set_position(newpos)
+        #    if i in homing_axes:
+        #        self.limits[i] = rail.get_range()
 
     def note_z_not_homed(self):
         # Helper for Safe Z Home
         # TODO: is this per axis or per stepper?
         self.limits[2] = (1.0, -1.0)
-
+    
     def home(self, homing_state):
-        # Each axis is homed independently and in order
-        # TODO: maybe home z first?
-        for axis in homing_state.get_axes():
-            rail = self.rails[axis]
-            # Determine movement
-            position_min, position_max = rail.get_range()
-            hi = rail.get_homing_info()
-            homepos = [None, None, None, None]
-            homepos[axis] = hi.position_endstop
-            forcepos = list(homepos)
-            if hi.positive_dir:
-                forcepos[axis] -= 1.5 * (hi.position_endstop - position_min)
-            else:
-                forcepos[axis] += 1.5 * (position_max - hi.position_endstop)
-            # Perform homing
-            homing_state.home_rails([rail], forcepos, homepos)
+        # Each axis is homed independently or all together?
+        logging.error("homing NOT IMPLEMENTED---------")
+        logging.error(homing_state.get_axes())
+        logging.error("---------homing")
+        # TODO - homing not implemented
+        homing_state.set_axes([0, 1, 2])
+        homing_state.set_homed_position([170.,170., 170.])
+        self.limits = self.ranges # TODO: limits should be one per axis, not per motor?
 
     def _motor_off(self, print_time):
-        self.limits = [(1.0, -1.0)] * 3  # CHECKME: are limits per axis or rail?
+        self.limits = [(1.0, -1.0)] * 4  # CHECKME: are limits per axis or rail?
 
     def _check_endstops(self, move):
         end_pos = move.end_pos
@@ -140,8 +184,8 @@ class MIRlikeKinematics:
         axes = [a for a, (l, h) in zip("xyz", self.limits) if l <= h]
         return {
             "homed_axes": "".join(axes),
-            "axis_minimum": self.axes_min,
-            "axis_maximum": self.axes_max,
+            "axis_minimum": 0, # TODO: self.axes_min,
+            "axis_maximum": 270 # TODO: self.axes_max,
         }
 
 
